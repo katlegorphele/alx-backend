b2148483424a57bedca3ac0d07ffbdd6251229d2
Kat-Devs <katlegophele95@gmail.com>
2024-02-06 10:28:48 +0200

    up to task 4

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+    console.log('Redis client connected')
+});
+
+client.on('error', (err) => {
+    console.log(`Something went wrong ${err}`)
+});
